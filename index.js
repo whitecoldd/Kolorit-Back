3a0a459e29dbc13558d991b866c1d506bf52e3cc
whitecoldd <buyukliwc@gmail.com>
2022-07-22 15:44:06 +0300

    fixed order categories uniqueness

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ dotenv.config()
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log('Connection Successful')).catch((err)=>{console.log(err)})
 app.use(cors());
-
 app.use(express.json())
+
+
+
 app.use('/api/auth', authRoute)
 app.use('/api/cat', catRoute)
 app.use('/api/slider', sliderRoute)
--- models/Categories.js
+++ models/Categories.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const CategoriesSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, unique: true },
-        img: {type: String, required: true, unique: true},
+        name: { type: String, required: true },
+        img: {type: String, required: true},
 
     },
     { timestamps: true }
