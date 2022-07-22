d910c3f5099fbd44e6b559a0785d2172bf5c9dcc
whitecoldd <buyukliwc@gmail.com>
2022-07-22 16:52:46 +0300

    ver.1

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ app.use('/api/article', articlesRoute)
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('Kolorit-Front/build'))
+    app.use(express.static('Kolorit-Admin/build'))
 
     app.get('*', (req, res)=>{
         res.sendFile(path.resolve(__dirname, 'Kolorit-Front', 'build', 'index.html'))
