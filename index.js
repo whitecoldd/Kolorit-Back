c282586b7e15ef425e3f6b033273b4441b8b30dd
whitecoldd <buyukliwc@gmail.com>
2022-07-22 16:48:54 +0300

    merged client, server and admin sides

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require ('mongoose')
 const dotenv = require ('dotenv')
 const cors = require('cors')
+const path = require ('path')
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
 const itemsRoute = require('./routes/items')
@@ -16,10 +17,9 @@ const articlesRoute = require('./routes/articles')
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log('Connection Successful')).catch((err)=>{console.log(err)})
+
 app.use(cors());
 app.use(express.json())
-
-
 
 app.use('/api/auth', authRoute)
 app.use('/api/cat', catRoute)
@@ -31,6 +31,14 @@ app.use('/api/contact', contactRoute)
 app.use('/api/order', orderRoute)
 app.use('/api/about', aboutRoute)
 app.use('/api/article', articlesRoute)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('Kolorit-Front/build'))
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'Kolorit-Front', 'build', 'index.html'))
+    })
+}
 
 app.listen(1000, ()=>{
     console.log('backend server is up')
