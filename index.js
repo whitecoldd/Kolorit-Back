5885ee75bc5cb62be4581b6b99599bc80c686fcc
whitecoldd <buyukliwc@gmail.com>
2022-07-23 17:14:30 +0300

    renewal

--- index.js
+++ index.js
@@ -32,14 +32,6 @@ app.use('/api/order', orderRoute)
 app.use('/api/about', aboutRoute)
 app.use('/api/article', articlesRoute)
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('Kolorit-Front/build'))
-    app.use(express.static('Kolorit-Admin/build'))
-
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'Kolorit-Front', 'build', 'index.html'))
-    })
-}
 
 app.listen(1000, ()=>{
     console.log('backend server is up')
