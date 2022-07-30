389baa2698397e0efe1dc70a86e58132dcdfc904
whitecoldd <buyukliwc@gmail.com>
2022-07-30 20:22:02 +0300

    re- v2.0.0

--- Kolorit-Front/src/requests/request.js
+++ Kolorit-Front/src/requests/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `https://kkolorit.herokuapp.com/`;
+const BASE_URL = `http://localhost:5000/`;
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
@@ -10,10 +10,10 @@ const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.PUBLIC_URL,
 });
 
 export const userRequest = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.PUBLIC_URL,
   headers: { token: `Bearer ${TOKEN}` },
 });
