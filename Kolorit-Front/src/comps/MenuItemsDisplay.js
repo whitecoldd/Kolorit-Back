0b2d49c604fe098ee14dbda7ca7d65c44c5e594f
whitecoldd <buyukliwc@gmail.com>
2022-08-17 13:17:50 +0300

    v3.3.5

--- Kolorit-Front/src/comps/MenuItemsDisplay.js
+++ Kolorit-Front/src/comps/MenuItemsDisplay.js
@@ -50,7 +50,10 @@ const MenuItemsDisplay = () => {
     <div className="hovwhite position-relative">
       <div className="scrollable-div-menu">
         {Cat?.map((Cat) => (
-          <div className="d-flex align-items-end dropdown2 mt-0 mb-0 pt-1 pb-1" type="button">
+          <div
+            className="d-flex align-items-end  dropdown2 mt-0 mb-0 pt-1 pb-1"
+            type="button"
+          >
             <img width={20} height={20} src={Cat.img} />
             <Nav.Link
               className="nav-fix d-flex align-items-center"
@@ -59,40 +62,44 @@ const MenuItemsDisplay = () => {
               <Link to={`/catalog/${Cat.name}`} className="real-no-dec">
                 <span>{Cat.name}</span>
               </Link>
-              <div className="dropdown-content-menu">
-                <h1 className="pt-1 ps-1">{Cat.name}</h1>
-                <div className="dropdown-content-all">
-                  {subcat
-                    .filter((subcat) =>
-                      [Cat.name].some((catname) =>
-                        [subcat.cat].flat().includes(catname)
+              <div className="wrapper">
+                <div className="dropdown-content-menu">
+                  <h1 className="pt-1 ps-1">{Cat.name}</h1>
+                  <div className="dropdown-content-all">
+                    {subcat
+                      .filter((subcat) =>
+                        [Cat.name].some((catname) =>
+                          [subcat.cat].flat().includes(catname)
+                        )
                       )
-                    )
-                    //?.filter((item) => item.lng === myLocalStorageData)
-                    .map((subcat) => {
-                      return (
-                        <div>
-                          <Link to={`/catalog/${Cat.name}/${subcat.name}`}>
-                            <h4>{subcat.name}</h4>
-                          </Link>
-                          {subsubcat
-                            .filter((subsubcat) =>
-                              [subcat.name].some((subname) =>
-                                [subsubcat.subcat].flat().includes(subname)
+                      //?.filter((item) => item.lng === myLocalStorageData)
+                      .map((subcat) => {
+                        return (
+                          <div>
+                            <Link to={`/catalog/${Cat.name}/${subcat.name}`}>
+                              <h4>{subcat.name}</h4>
+                            </Link>
+                            {subsubcat
+                              .filter((subsubcat) =>
+                                [subcat.name].some((subname) =>
+                                  [subsubcat.subcat].flat().includes(subname)
+                                )
                               )
-                            )
-                            .map((subsubcat) => {
-                              return (
-                                <Container>
-                                  <Link to={`/catalog/title/${subcat.name}/${subsubcat.name}`}>
-                                    <p className="mb-0">{subsubcat.name}</p>
-                                  </Link>
-                                </Container>
-                              );
-                            })}
-                        </div>
-                      );
-                    })}
+                              .map((subsubcat) => {
+                                return (
+                                  <Container>
+                                    <Link
+                                      to={`/catalog/title/${subcat.name}/${subsubcat.name}`}
+                                    >
+                                      <p className="mb-0">{subsubcat.name}</p>
+                                    </Link>
+                                  </Container>
+                                );
+                              })}
+                          </div>
+                        );
+                      })}
+                  </div>
                 </div>
               </div>
             </Nav.Link>
